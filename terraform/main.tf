4184e440a3279a4ec0844749f3b922dec3d57d59
peterclark1996@gmail.com <peterclark1996@gmail.com>
2022-06-13 23:19:56 +0100

    Update to D1 tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_service_plan" "main" {
   location = var.location
   resource_group_name = azurerm_resource_group.main.name
 
-  sku_name = "F1"
+  sku_name = "D1"
   os_type = "Linux"
 }
 
