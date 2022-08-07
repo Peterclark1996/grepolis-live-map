558de67d1911f4ddfe56650b516326352ff6e76c
peterclark1996@gmail.com <peterclark1996@gmail.com>
2022-08-07 20:42:24 +0100

    Add logic to save data to blob container

--- func/RefreshData/blob.ts
+++ func/RefreshData/blob.ts
@@ -1,3 +1,19 @@
 import { WorldData } from "./types/WorldData"
+import { BlobServiceClient } from '@azure/storage-blob'
 
-export const saveWorldDataFile = (worldCode: string, fileName: string, worldData: WorldData) => Promise.resolve()
+const WORLD_DATA_CONTAINER_NAME = "world-data"
+
+export const saveWorldDataFile = async (worldCode: string, fileName: string, worldData: WorldData) => {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION)
+    const containerClient = blobServiceClient.getContainerClient(WORLD_DATA_CONTAINER_NAME)
+
+    const containerExists = await containerClient.exists()
+
+    if (!containerExists) {
+        await containerClient.create()
+    }
+
+    const blockBlobClient = containerClient.getBlockBlobClient(`${worldCode}/${fileName}.json`)
+    const dataToSave = JSON.stringify(worldData)
+    await blockBlobClient.upload(dataToSave, dataToSave.length)
+}
