33a5d5cbeec5383118eb9a722fd3a6b43e868bd9
peterclark1996@gmail.com <peterclark1996@gmail.com>
2022-08-29 17:54:40 +0100

    Update workflow to get sa connection string from terraform output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
+output "storage_account_connection_string" {
+  value = azurerm_storage_account.main.primary_connection_string
+  sensitive = true
+}
+
 output "storage_account_url" {
   value = azurerm_storage_account.main.primary_blob_endpoint
   sensitive = true
